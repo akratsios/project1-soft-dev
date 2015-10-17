c41b67ca4c48fbc2042653994c539c26105a9702
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2015-10-17 18:02:40 -0400

    Can add blogs and comments

--- utils.py
+++ utils.py
@@ -3,17 +3,31 @@ import sqlite3
 connection = sqlite3.connect("database.db")
 c = connection.cursor()
 
-def addpost(t, ID, un, post):
-    #inserts new post into the blogs table
-    TEMPLATE = "INSERT INTO blogs VALUES (\"%(title)s\", %(BID)s, \"%(username)s\", \"%(post)s\");"
-    q=TEMPLATE%({"title":t, "BID":ID, "username":un, "post":post})
-    print q
+def addblog(t, ID, un, post):    
+    #inserts new blog into the blogs table
+    connection = sqlite3.connect("database.db")
+    c = connection.cursor()
+    #just in case it's the first blog ever
+    c.execute("CREATE TABLE IF NOT EXISTS blogs (title TEXT, BID INTEGER, username TEXT, post TEXT)")
 
-    #currently c.execute(q) is giving us an error
+    TEMPLATE = "INSERT INTO blogs VALUES (\"%(title)s\", %(BID)s, \"%(username)s\", \"%(post)s\");"
+    q = TEMPLATE%({"title":t, "BID":ID, "username":un, "post":post})
     c.execute(q)
 
-c.execute("CREATE TABLE IF NOT EXISTS blogs (title TEXT, BID INTEGER, username TEXT, post TEXT)")
-addpost("kek", 69, "RarestPepe", "hey everyone it's a me Mario")
-c.execute("SELECT * FROM blogs")
+def addcomment(CID, BID, un, cmt):
+    #inserts new comment into the comments table
+    connection = sqlite3.connect("database.db")
+    c = connection.cursor()
+    #just in case it's the first comment ever
+    c.execute("CREATE TABLE IF NOT EXISTS comments (CID INTEGER, BID INTEGER, username TEXT, comment TEXT)")
+    
+    TEMPLATE = "INSERT INTO comments VALUES(%(CID)s, %(BID)s, \"%(username)s\", \"%(comment)s\");" 
+    q = TEMPLATE%({"CID":CID, "BID":BID, "username":un, "comment":cmt})
+    c.execute(q)
+
+
+
+addcomment(1, 69, "RarestPepe", "hey everyone it's a me Mario")
+
 
 
